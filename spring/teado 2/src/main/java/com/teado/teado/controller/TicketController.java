071e6919a4414cbdb4d6667341f8039ae12d90a0
Bee <69877469+bee-dmg@users.noreply.github.com>
2023-01-06 17:14:54 -0500

    Finishing controller

--- spring/teado 2/src/main/java/com/teado/teado/controller/TicketController.java
+++ spring/teado 2/src/main/java/com/teado/teado/controller/TicketController.java
@@ -4,6 +4,7 @@ package com.teado.teado.controller;
 import com.teado.teado.model.Ticket;
 import com.teado.teado.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,29 @@ public class TicketController {
     public ResponseEntity<List<Ticket>> getAllTickets(@RequestParam(required = false) String title){
 
     }
-    @
+    @GetMapping("/tickets/{id}")
+    public ResponseEntity<Ticket> getTicketById(@PathVariable("id") long id){
+
+    }
+    @PostMapping("/tickets")
+    public ResponseEntity<Ticket> createTicket(@RequestBody Ticket ticket){
+
+    }
+
+    @PutMapping("/tickets/{id}")
+    public ResponseEntity<Ticket> updateTicket(@PathVariable("id") long id, @RequestBody Ticket ticket){
+
+    }
+    @DeleteMapping("/tickets/{id}")
+    public ResponseEntity<HttpStatus> deleteTicket(@PathVariable("id") long id){
+
+    }
+    @DeleteMapping("/tickets")
+    public ResponseEntity<HttpStatus> deleteAllTickets(){
+
+    }
+
+
+
 
 }
