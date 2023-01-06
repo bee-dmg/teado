b8d49f79f11e4779e7c4dfc3582afea9242529d6
Bee <69877469+bee-dmg@users.noreply.github.com>
2023-01-06 17:32:24 -0500

    Flushing out controller.

--- spring/teado 2/src/main/java/com/teado/teado/controller/TicketController.java
+++ spring/teado 2/src/main/java/com/teado/teado/controller/TicketController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins="http://localhost:8081")
@@ -19,12 +20,26 @@ public class TicketController {
     TicketRepository ticketRepository;
 
     @GetMapping("/tickets")
-    public ResponseEntity<List<Ticket>> getAllTickets(@RequestParam(required = false) String title){
+    public ResponseEntity<List<Ticket>> getAllTickets(@RequestParam(required = false) String title) {
+        try {
+            List<Ticket> tickets = new ArrayList<Ticket>();
+            if (title == null) {
+                ticketRepository.findAll().forEach(tickets::add);
+            } else {
+                ticketRepository.findByTitleContaining(title).forEach(tutorials:add);
+            }
 
+            if (tickets.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tickets, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     @GetMapping("/tickets/{id}")
     public ResponseEntity<Ticket> getTicketById(@PathVariable("id") long id){
-
+        
     }
     @PostMapping("/tickets")
     public ResponseEntity<Ticket> createTicket(@RequestBody Ticket ticket){
