28d2ab66a95ff5be44744f08357ca2e8dc678026
Bee <69877469+bee-dmg@users.noreply.github.com>
2023-01-05 20:38:42 -0500

    adding controller.

---
+++ spring/teado 2/src/main/java/com/teado/teado/controller/TicketController.java
@@ -0,0 +1,23 @@
+package com.teado.teado.controller;
+
+
+import com.teado.teado.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins="http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class TicketController {
+
+    @Autowired
+    TicketRepository ticketRepository;
+
+    @GetMapping("/tickets")
+    public ResponseEntity<List<Ticket>> getAllTickets(@RequestParam(required = false) int id{
+        
+    }
+
+}
