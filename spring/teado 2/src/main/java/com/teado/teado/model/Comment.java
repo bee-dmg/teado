c27ea12e2a47c0e8ea06166b545da46314994289
Bee <69877469+bee-dmg@users.noreply.github.com>
2023-02-25 11:09:17 -0500

    adding comments

--- spring/teado 2/src/main/java/com/teado/teado/model/Comment.java
+++ spring/teado 2/src/main/java/com/teado/teado/model/Comment.java
@@ -25,6 +25,14 @@ public class Comment {
     }
 
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
